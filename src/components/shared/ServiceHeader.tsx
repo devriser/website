93507eca3d4bf8434fd6d3c1c4d649d28cdcb150
rahatDev007 <rahat.sikder@devriser.com>
2023-12-12 12:40:08 +0600

    Updated WebDev Contents

--- src/components/shared/ServiceHeader.tsx
+++ src/components/shared/ServiceHeader.tsx
@@ -17,7 +17,7 @@ interface serviceHeaderProps {
 const ServiceHeader = ({ title, description }: serviceHeaderProps) => {
   return (
     <div className='flex flex-col gap-4 items-center text-center'>
-      <h2 className='text-3xl font-semibold'>{title}</h2>
+      <h2 className='text-3xl font-semibold '>{title}</h2>
       {description && <p className='text-[#A1A1A1] max-w-4xl'>{description}</p>}
     </div>
   );
