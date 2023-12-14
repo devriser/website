0f01d8ad61b853a36b60eee70035241f146aee1c
rahilansari261 <rahil.ansari@devriser.com>
2023-12-14 16:14:54 +0530

    fixes

--- src/components/shared/Button/types.ts
+++ src/components/shared/Button/types.ts
@@ -10,10 +10,9 @@ export type Button = {
   className?: string;
   onClick?: any;
   buttonSize?: "full" | "auto";
-  dropdownButtons?: Button;
   href?: any;
-  loading?:boolean;
-} & (ButtonProps | NextLinkProps | SingleDropdownMenu | DualButton);
+  loading?: boolean;
+} & (ButtonProps | NextLinkProps);
 
 type ButtonProps = {
   as?: "Button";
@@ -25,31 +24,12 @@ interface NextLinkProps extends LinkProps {
   as?: "NextLink";
 }
 
-interface Option {
-  iconSVG?: any;
-  name: string;
-  value: string;
-  onClick?: () => void;
-  itemType?: string;
-}
-
-type SingleDropdownMenu = {
-  as?: "SingleDropdownMenu";
-  options: Option[];
-  isOpen: boolean;
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>> | (() => void);
-  opener:"Button" | "ThreeDots"; // this will be only Button
-};
-
-type DualButton = {
-  as?: "DualButton";
-  options: Option[];
-  isOpen: boolean;
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>> | (() => void);
-  opener:"Button";
-};
 export type VariantClasses = {
-  [key in NonNullable<Button["variant"]>]: { solid: string; outlined?: string; transparent?: string };
+  [key in NonNullable<Button["variant"]>]: {
+    solid: string;
+    outlined?: string;
+    transparent?: string;
+  };
 };
 export type SizeClasses = {
   [key in NonNullable<Button["size"]>]: string;
--- src/components/shared/TitleHeader.tsx
+++ src/components/shared/TitleHeader.tsx
@@ -1,15 +1,31 @@
 import React from "react";
+import Button from "./Button";
 
 interface HeaderProps {
   title: string;
   description: string;
+  btnText1: string;
+  btnText2: string;
+  btnHref1: string;
+  btnHref2: string;
 }
 
-const TitleHeader = ({ title, description }: HeaderProps) => {
+const TitleHeader = ({
+  title,
+  description,
+  btnText1,
+  btnText2,
+  btnHref1,
+  btnHref2,
+}: HeaderProps) => {
   return (
-    <div className='flex flex-col gap-4 justify-start'>
-      <h2 className='text-3xl font-semibold text-secondary-reverse'>{title}</h2>
-      <p className='text-xl text-secondary-reverse'>{description}</p>
+    <div className="flex flex-col gap-4 justify-start">
+      <h2 className="text-3xl font-semibold text-secondary-reverse">{title}</h2>
+      <p className="text-xl text-secondary-reverse">{description}</p>
+      <div className="flex gap-6 justify-start mt-10">
+        <Button as="NextLink" href={btnHref1} variant="success" style="outlined">{btnText1}</Button>
+        <Button as="NextLink" href={btnHref2} variant="success" style="solid">{btnText2}</Button>
+      </div>
     </div>
   );
 };
